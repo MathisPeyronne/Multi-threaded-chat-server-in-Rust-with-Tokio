5ca131e4ae1f419145be67c2e1e3edbc67ec7e08
Mathis Peyronne <mathis.p@live.com>
2023-01-08 18:58:38 +0100

    first_commit

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,14 @@ mod http;
 mod server;
 mod website_handler;
 
+
+
 fn main() {
     let default_path = format!("{}\\public", env!("CARGO_MANIFEST_DIR"));
     let public_path = env::var("PUBLIC_PATH").unwrap_or(default_path);
     println!("public_path: {}", public_path);
     let server = Server::new(
-        "127.0.0.1:8080".to_string(),
+        "0.0.0.0:8080".to_string(),
         Arc::new(Mutex::new(vec![(
             "Welcome to this chat server :)".to_string(),
             "Admin".to_string(),
